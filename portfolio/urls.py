2f675d24b573b692d17e5a65d8492356497d2a24
diego-gv <diegosalvador.gv@gmail.com>
2022-11-27 16:19:43 +0100

    Añadido enlace al panel de adminitración si el usuario es staff

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
@@ -25,8 +26,9 @@ urlpatterns = [
     path('account/', include('apps.users.urls')),
 ]
 
-# SECURITY WARNING: don't run with this bellow code on in production!
-from django.conf import settings
-from django.conf.urls.static import static
+if settings.DEBUG:
+    # SECURITY WARNING: don't run with this bellow code on in production!
+    from django.conf import settings
+    from django.conf.urls.static import static
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
