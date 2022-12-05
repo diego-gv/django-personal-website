82ea37131e1f8769f5865ec6eab45b477fbe61cc
diego-gv <diegosalvador.gv@gmail.com>
2022-12-05 12:06:06 +0100

    Añadido formulario y vista de registro de usuarios

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -5,4 +5,5 @@ from apps.users import views
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('dashboard/', views.dashboard, name="dashboard"),
+    path('register/', views.register, name="register"),
 ]
